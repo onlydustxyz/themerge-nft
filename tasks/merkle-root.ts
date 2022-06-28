423629baf6c17b2d43ad90a4c830cb822dc3a117
Stanislas Bernard <bernardstanislas@gmail.com>
2022-06-28 22:16:05 +0200

    refactor: create a whitelist reader to share it between tasks

--- tasks/merkle-root.ts
+++ tasks/merkle-root.ts
@@ -1,25 +1,12 @@
-import { readFile } from "fs/promises";
 import { task } from "hardhat/config";
 import { TaskArguments } from "hardhat/types";
 import { ethers } from "ethers";
 import { keccak256 } from "ethers/lib/utils";
 import MerkleTree from "merkletreejs";
-
-export type WhiteList = {
-  address: string;
-  packedTypes: number;
-}[];
-
-export const readWhitelist = async (path: string): Promise<WhiteList> => {
-  const whitelist = JSON.parse(await readFile(path, "utf-8"));
-  return whitelist.map(({ address, nft }: { address: string; nft: string }) => ({
-    address,
-    packedTypes: parseInt(nft, 16),
-  }));
-};
+import { readWhitelist, WhiteList } from "./whitelist-reader";
 
 export const generateWhitelistMerkleTree = (whitelist: WhiteList): MerkleTree => {
-  const leaves = whitelist.map(({ address, packedTypes }) => generateLeafData(address, unpackNftTypes(packedTypes)));
+  const leaves = whitelist.map(({ address, nftTypes }) => generateLeafData(address, nftTypes));
   const whitelistMerkleTree = new MerkleTree(leaves, keccak256, {
     sortPairs: true,
   });
@@ -32,18 +19,6 @@ const generateLeafData = (address: string, types: number[]): string => {
 
 const toBytes32 = (value: number): string => {
   return ethers.utils.hexZeroPad(ethers.utils.hexlify(value), 32);
-};
-
-export const unpackNftTypes = (packedValue: number): number[] => {
-  const result = [];
-  let currentBit = 0;
-  while (2 ** currentBit <= packedValue) {
-    if (packedValue & (2 ** currentBit)) {
-      result.push(currentBit);
-    }
-    currentBit++;
-  }
-  return result;
 };
 
 export const getMerkleRoot = async (pathToWhitelist: string): Promise<string> => {
